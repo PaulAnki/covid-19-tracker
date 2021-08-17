2919ef86310681d1d62e1d00ae40deabe149eaab
PaulAnki <ankita17iima@gmail.com>
2021-08-18 00:07:37 +0530

    Updates

---
+++ precache-manifest.4e419d6ba0b982eb6e1e9e8bd9d87bd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2985fe6738148746044ee55d053066c1",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "01ff27e02219205f7b9b",
+    "url": "/covid-19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "117d5bac33b9386a7fb8",
+    "url": "/covid-19-tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "01ff27e02219205f7b9b",
+    "url": "/covid-19-tracker/static/js/2.7c300910.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid-19-tracker/static/js/2.7c300910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "117d5bac33b9386a7fb8",
+    "url": "/covid-19-tracker/static/js/main.64453fb7.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  }
+]);
